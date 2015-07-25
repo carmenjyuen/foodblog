1b2957c4b78295d354476d4ef7e72afb65618deb
carmen yuen <carmenyuen@Matts-MacBook-Air.local>
2015-07-25 13:49:41 -0700

    first commit for foodblog

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "rubygems"
+require "sinatra"
+
+set :static, true
+set :public, File.dirname(__FILE__) + "/public"
+
+get "/" do 
+	@entries = (1..100).map { |i| "<div class='blogentry'>
+	<div id='title'> Sushi #{i} </div>
+	<div id='video'>
+		<iframe width='560' height='315' src='https://www.youtube.com/embed/tbsFAEz5vok?showinfo=0' frameborder='0' allowfullscreen></iframe>
+	</div>
+	<div id='post_text'></div>
+</div>"}
+	erb:foodblog_template
+end
+
