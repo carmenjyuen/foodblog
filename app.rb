75bd39945be2a0ed1aac7ea88e065d7b8ea9ba16
carmen yuen <carmenjyuen@gmail.com>
2015-07-25 14:23:40 -0700

    added stuff for heroku deploy

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ set :static, true
 set :public, File.dirname(__FILE__) + "/public"
 
 get "/" do 
-	@entries = (1..100).map { |i| "<div class='blogentry'>
+	@entries = (1..20).map { |i| "<div class='blogentry'>
 	<div id='title'> Sushi #{i} </div>
 	<div id='video'>
 		<iframe width='560' height='315' src='https://www.youtube.com/embed/tbsFAEz5vok?showinfo=0' frameborder='0' allowfullscreen></iframe>
