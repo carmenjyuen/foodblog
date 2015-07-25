0f1cdd2457d8fddb8fb017802f6ce7db44534b3d
carmen yuen <carmenjyuen@gmail.com>
2015-07-25 14:29:49 -0700

    heroku shenanigans

--- app.rb
+++ app.rb
@@ -1,17 +1,19 @@
 require "rubygems"
 require "sinatra"
 
-set :static, true
-set :public, File.dirname(__FILE__) + "/public"
+class App < Sinatra::Base
+	set :static, true
+	set :public, File.dirname(__FILE__) + "/public"
 
-get "/" do 
-	@entries = (1..20).map { |i| "<div class='blogentry'>
+	get "/" do 
+		@entries = (1..20).map { |i| "<div class='blogentry'>
 	<div id='title'> Sushi #{i} </div>
 	<div id='video'>
 		<iframe width='560' height='315' src='https://www.youtube.com/embed/tbsFAEz5vok?showinfo=0' frameborder='0' allowfullscreen></iframe>
 	</div>
 	<div id='post_text'></div>
 </div>"}
-	erb:foodblog_template
+		erb:foodblog_template
+	end
 end
 
